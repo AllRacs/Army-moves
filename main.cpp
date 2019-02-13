778008b3122a889fa7dcd05829c87953716d97cb
AllRacs <oscarplate@gmail.com>
2019-02-13 18:38:53 +0100

    Circle shape code example

--- main.cpp
+++ main.cpp
@@ -1,37 +1,21 @@
 #include <SFML/Graphics.hpp>
-
 int main()
 {
-    // Create the main window
-    sf::RenderWindow app(sf::VideoMode(800, 600), "SFML window");
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("cb.bmp"))
-        return EXIT_FAILURE;
-    sf::Sprite sprite(texture);
-
-	// Start the game loop
-    while (app.isOpen())
+    sf::RenderWindow window(sf::VideoMode(1240, 720), "SFML works!");
+    window.setPosition({100,100});
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    while (window.isOpen())
     {
-        // Process events
         sf::Event event;
-        while (app.pollEvent(event))
+        while (window.pollEvent(event))
         {
-            // Close window : exit
             if (event.type == sf::Event::Closed)
-                app.close();
+            window.close();
         }
-
-        // Clear screen
-        app.clear();
-
-        // Draw the sprite
-        app.draw(sprite);
-
-        // Update the window
-        app.display();
+        window.clear();
+        window.draw(shape);
+        window.display();
     }
-
-    return EXIT_SUCCESS;
+    return 0;
 }
